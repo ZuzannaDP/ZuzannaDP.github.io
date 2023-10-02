60d6ffae8bf89ea6b267dc88b699f72d21102d55
Zuzanna <zuzanna.ponia@gmail.com>
2023-10-02 22:37:59 +0100

    add footer with copyright

--- web-ui/src/home/Home.js
+++ web-ui/src/home/Home.js
@@ -4,6 +4,16 @@ import Projects from '../projects/Projects';
 import './Home.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+function Footer() {
+  let year = new Date().getFullYear();
+
+  return (
+    <footer>
+      &copy; Copyright <em>{year}</em>. Made by Zuzanna.
+    </footer>
+  )
+}
+
 function Home() {
   return (
     <BrowserRouter>
@@ -15,6 +25,7 @@ function Home() {
             <Route path='/projects/*' element={<Projects />}></Route>
           </Routes>
         </div>
+        <Footer />
     </BrowserRouter>
   );
 }
